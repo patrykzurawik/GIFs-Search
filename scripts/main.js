b9b2971736895fef87467d4bca582b55fc5f208b
patrykzurawik <pzurawik@wi.zut.edu.pl>
2016-10-20 14:17:48 +0200

    GIFs Search #5
    sticky searchBar; scrollUpButton modified; little tidiness done

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,22 @@
+$(document).ready(function()
+{
+	var searchBarY = $('#searchBar').offset().top;
+	var stickySearchBar = function()
+	{
+		var scrollY = $(window).scrollTop();
+		if (scrollY > searchBarY) $('#searchBar').addClass('sticky');
+		else $('#searchBar').removeClass('sticky');
+	};
+	
+	stickySearchBar();
+
+	$(window).scroll(function()
+	{
+		stickySearchBar();
+	}
+	);
+});
+
 $(window).scroll(function()
 {
 	if($(this).scrollTop()>450) $('#scrollUpButton').fadeIn();
